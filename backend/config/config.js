4d7e87fda7de0cd9eaedb1003cfab4d97a899fb0
Kyle Vertodoulos <kvertodoulos@harding.edu>
2026-03-05 00:58:07 -0600

    Remove config.js

--- backend/config/config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const projectDir = path.join(__dirname, "..");
-
-function projectPath(...localPaths) {
-  return path.join(projectDir, ...localPaths);
-}
-
-module.exports = { projectPath };
